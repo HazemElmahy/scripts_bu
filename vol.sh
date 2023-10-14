1cf05d78c98a85e04440109d7455cef7585aa4b1
hmahy <hmahy.abuerdan.com>
2023-10-14 11:14:55 +0300

    add vol level to vol script

--- vol.sh
+++ vol.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 pactl -- set-sink-volume 0 ${1}
+level=$(pactl list sinks | grep "front-right:" | awk -F'[/,%]' '{print "Left:"$2"%\nRight:"$6"%"}')
+echo 
 
-notify-send -i "/home/hazem/dotfiles/assets/volume.png" -r 1 -t 1000 "Volume" " ${1}"
+notify-send \
+    -i "/home/hazem/dotfiles/assets/volume.png" \
+    -r 1 -t 1000 "Volume" "${level} \n ${1}"
